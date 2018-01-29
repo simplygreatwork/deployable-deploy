3a14879d9097e27fe50f6c8176092c6f34292bd3
Philip Weaver <pweaver@windhoverlabs.com>
2018-01-29 00:49:12 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+
+```
+npm install -g @kelda/install
+kelda init
+kelda daemon
+npm install
+kelda run ./deploy.js
+```
+
+- http://docs.kelda.io/#quick-start
---
+++ deploy.js
@@ -0,0 +1,71 @@
+
+const kelda = require('kelda');
+
+Deployment = function(options) {
+	
+	this.initialize(options);
+};
+
+Deployment.prototype = {
+	
+	initialize : function(options) {
+		
+		Object.assign(this, options);
+		this.port = this.port || 80;
+		this.scale = this.scale || 1;
+		this.environment = {
+			PORT: this.port.toString()
+		};
+		this.name = this.name || this.appname(this.repository);
+		var dockerfile = this.dockerfile(this.repository);
+		const image = new kelda.Image(this.name, dockerfile);
+		this.containers = [];
+		for (let i = 0; i < this.scale; i++) {
+			this.containers.push(new kelda.Container(this.name, image, {
+				command: this.command()
+			}).withEnv(this.environment));
+		}
+		kelda.allow(kelda.publicInternet, this.containers, this.port);
+		const infrastructure = kelda.baseInfrastructure();
+		this.containers.forEach(function(container) {
+			container.deploy(infrastructure);
+		});
+	},
+	
+	appname : function(repository) {
+		
+		let result = 'application';
+		const index = repository.lastIndexOf('/');
+		if (index !== -1) {
+			result = repository.slice(index + 1);
+		}
+		return result;
+	},
+	
+	dockerfile : function(repository) {
+		
+		var array = [];
+		array.push('FROM node:8.9.4');
+		array.push('RUN mkdir -p /usr/src/app');
+		array.push('WORKDIR /usr/src/app');
+		array.push('RUN git clone ' + repository + ' .');
+		array.push('RUN npm install');
+		return array.join('\r\n');
+	},
+	
+	command : function() {
+		
+		if (true) {
+			return ['npm', 'start'];
+		} else {
+			return ['tail', '-f', '/dev/null'];
+		}
+	}
+};
+
+const deployment = new Deployment({
+	// name : 'application',
+	repository: 'https://bitbucket.org/philmaker/deployable',
+	port : 80,
+	scale: 1
+});
